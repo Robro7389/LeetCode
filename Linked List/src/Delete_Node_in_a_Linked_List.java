a256b575908670ef2457e1d6787fa7a2af661f72
Ronak Gour <ronakgour7389@gmail.com>
2024-08-29 16:22:13 +0530

    Q

--- Linked List/src/Delete_Node_in_a_Linked_List.java
+++ Linked List/src/Delete_Node_in_a_Linked_List.java
@@ -1,6 +1,10 @@
 public class Delete_Node_in_a_Linked_List {
     //Leetcode 237. Delete Node in a Linked List
 
+    //How to become another person in the world? Two steps.
+    //One, change your appearance to whom you want to be.
+    //Two, kill that person.
+
     public class ListNode{
         ListNode next;
         int val;
